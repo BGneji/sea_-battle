956324ebf10b82de509d4c0822653b732bb11ad6
NEJI <mrkochetov@yandex.ru>
2023-06-07 11:25:53 +0300

    play area

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ app_running = True
 # размеры окна
 size_canvas_x = 600
 size_canvas_y = 600
+s_x = s_y = 9  # размер игрового поля
+# размер шагов между ячейками
+step_x = size_canvas_x // s_x  # шаг по горизонтали
+step_y = size_canvas_y // s_y  # шаг по вертикали
+# переопределим ширину и высоту окна игры чтобы не было зазоров по бокам
+size_canvas_x = step_x*s_x
+size_canvas_y = step_y*s_y
+
+# добавим к игровому полю еще пространство, что бы не нарушалось целостность игрового поля
+menu_x = 250
+
+
 
 
 def on_closing():
@@ -34,7 +46,7 @@ tk.resizable(0, 0)
 # окно игры поверх всех окон
 tk.wm_attributes("-topmost", 1)
 # окно игры с нашими параметрами которые мы задавали раньше
-canvas = Canvas(tk, width=size_canvas_x, height=size_canvas_y, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width=size_canvas_x+menu_x, height=size_canvas_y, bd=0, highlightthickness=0)
 # создаем окно в виде прямоугольника заливка(fill)
 canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill='white')
 # упаковка нашего окна
@@ -42,6 +54,31 @@ canvas.pack()
 # обновление нашего объекта
 tk.update()
 
+
+# функция, которая рисует поля игры
+def draw_table():
+    for i in range(0, s_x + 1):
+        canvas.create_line(step_x * i, 0, step_x*i, size_canvas_y)
+    for i in range(0, s_x + 1):
+        canvas.create_line(0, step_y * i, size_canvas_x, step_y*i)
+
+
+draw_table()
+
+def button_show_enemy():
+    pass
+
+def button_begin_again():
+    pass
+
+
+# добавляем кнопки
+b0 = Button(tk, text='Показать коробли противника', command=button_show_enemy)
+# добавляем кнопки на панель и смещаем их чтобы они небыли на игровом поле
+b0.place(x=size_canvas_x+20, y=30)
+# добавляем кнопки на панель и смещаем их чтобы они небыли на игровом поле
+b1 = Button(tk, text="Начать заново!", command=button_begin_again)
+b1.place(x=size_canvas_x+20, y=70)
 
 # цикл для работы нашего приложения
 while app_running:
